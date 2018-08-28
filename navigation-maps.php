5ecf8025c2abe5dc9e9152748e99de60dab4367f
BrunoBoehm <bruno.boehm@gmail.com>
2018-08-28 18:29:35 +0200

    structure the plugin with comments

--- navigation-maps.php
+++ navigation-maps.php
@@ -6,4 +6,52 @@ Description: Navigation Maps for COMMEUNSEULHOMME missions.
 Version: 1.0
 */
 
+if ( !class_exists("LyNavMap") ) {
+    class LyNavMap {
+
+        // when installing the plugin
+        function map_install() {
+
+        }
+
+        // when desinstalling the plugin
+        function map_uninstall() {
+
+        }
+
+        // actions at initialization: add to admin menu
+        function init() {
+
+        }
+
+        // designing the admin interface
+        function map_admin_page() {
+
+        }
+
+        // requiring backend files, styles, js
+        function map_backend_imports() {
+
+        }
+
+        // requiring frontend files, styles, js
+        function map_frontend_imports() {
+            
+        }
+
+        // defining shortcode
+        function map_shortcode() {
+
+        }
+
+        // CRUD: SQL INSERT (Create)
+        // CRUD: SQL SELECT (Read)
+        // CRUD: SQL UPDATE (Update)
+        // CRUD: SQL DELETER (Delete)
+
+    }
+}
+
+// action hooks
+
 ?>
